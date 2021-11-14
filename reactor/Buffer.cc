436116bb9d48bbe1cdb9efb320455b715604e055
AhDai1 <1305377980@qq.com>
2021-11-14 15:46:01 +0800

    添加一些注释

--- reactor/Buffer.cc
+++ reactor/Buffer.cc
@@ -11,19 +11,19 @@ inline To implicit_cast(From const &f) {
 }
 ssize_t Buffer::readFd(int fd, int* savedErrno)
 {
-  char extrabuf[65536];
+  char extrabuf[65536];//栈上空间
   struct iovec vec[2];
   const size_t writable = writeableBytes();
-  vec[0].iov_base = begin()+writerIndex_;
+  vec[0].iov_base = begin()+writerIndex_;//Buffer
   vec[0].iov_len = writable;
-  vec[1].iov_base = extrabuf;
+  vec[1].iov_base = extrabuf;//stack
   vec[1].iov_len = sizeof extrabuf;
   const ssize_t n = readv(fd, vec, 2);
   if (n < 0) {
     *savedErrno = errno;
-  } else if (implicit_cast<size_t>(n) <= writable) {
+  } else if (implicit_cast<size_t>(n) <= writable) {//如果Buffer可以直接读入，就直接读
     writerIndex_ += n;
-  } else {
+  } else {//如果数据过大则先读到栈上，接着添加到Buffer的后面
     writerIndex_ = buffer_.size();
     append(extrabuf, n - writable);
   }
